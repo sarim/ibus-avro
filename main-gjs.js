a196bd1b9f8a7174ee994909894c2f6f72b6e9bc
Sarim Khan <sarim2005@gmail.com>
2012-06-26 11:06:56 +0600

    indention

--- main-gjs.js
+++ main-gjs.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env gjs
+
 const IBus = imports.gi.IBus;
 const GLib = imports.gi.GLib;
 const GObject = imports.gi.GObject;
@@ -14,23 +15,23 @@ IBus.init();
 //get the ibus bus
 var bus = new IBus.Bus();
 
-if( bus.is_connected() ) {
+if (bus.is_connected()) {
 
     var id = 0;
 
     function _create_engine_cb(factory, engine_name) {
 
         id += 1;
-        var engine =  new IBus.Engine({
+        var engine = new IBus.Engine({
             engine_name: engine_name,
             object_path: '/org/freedesktop/IBus/Engine/' + id,
             connection: bus.get_connection()
         });
 
-        engine.connect('process-key-event', function(engine,keyval,keycode,state) {
+        engine.connect('process-key-event', function (engine, keyval, keycode, state) {
 
             //print keypress infos, helpful for debugging
-            print(keyval + " " +  keycode + " " + state);
+            print(keyval + " " + keycode + " " + state);
 
             // ignore release event
             if (!(state == 0 || state == 1 || state == 16 || state == 17)) {
@@ -48,13 +49,12 @@ if( bus.is_connected() ) {
                 let bntext = Avroparser.parse(engine.buffertext);
                 bntext = utfconv.utf8Decode(bntext);
                 let text = IBus.Text.new_from_string(bntext);
-                engine.update_preedit_text(text,bntext.length,true);
+                engine.update_preedit_text(text, bntext.length, true);
 
                 let entext = IBus.Text.new_from_string(engine.buffertext);
-                engine.update_auxiliary_text(entext,true);
+                engine.update_auxiliary_text(entext, true);
                 return true;
-            }
-            else if (keyval == IBus.Return || keyval == IBus.space) {
+            } else if (keyval == IBus.Return || keyval == IBus.space) {
 
                 let bntext = Avroparser.parse(engine.buffertext);
                 bntext = utfconv.utf8Decode(bntext);
@@ -65,7 +65,7 @@ if( bus.is_connected() ) {
                 engine.hide_auxiliary_text();
 
                 if (keyval == IBus.space) {
-                    engine.commit_text( IBus.Text.new_from_string(" "));
+                    engine.commit_text(IBus.Text.new_from_string(" "));
                     return true;
                 }
                 /* i forgot what this commented block is here for !
@@ -75,20 +75,18 @@ if( bus.is_connected() ) {
                     return true;
                 }
                 */
-            }
-            else if (keyval == IBus.BackSpace){
+            } else if (keyval == IBus.BackSpace) {
                 if (engine.buffertext.length > 0) {
-                    engine.buffertext = engine.buffertext.substr(0,engine.buffertext.length -1);
+                    engine.buffertext = engine.buffertext.substr(0, engine.buffertext.length - 1);
                     let bntext = Avroparser.parse(engine.buffertext);
                     bntext = utfconv.utf8Decode(bntext);
                     let text = IBus.Text.new_from_string(bntext);
-                    engine.update_preedit_text(text,bntext.length,true);
+                    engine.update_preedit_text(text, bntext.length, true);
                     let entext = IBus.Text.new_from_string(engine.buffertext);
-                    engine.update_auxiliary_text(entext,true);
+                    engine.update_auxiliary_text(entext, true);
                     return true;
                 }
-            }
-            else if (keyval == IBus.Left || keyval == IBus.Right || keyval == IBus.Up || keyval == IBus.Down || keyval == IBus.Control_L || keyval == IBus.Control_R || keyval == IBus.Insert   || keyval == IBus.Delete || keyval == IBus.Home || keyval == IBus.Page_Up || keyval == IBus.Page_Down || keyval == IBus.End || keyval == IBus.Alt_L || keyval == IBus.Alt_R ){
+            } else if (keyval == IBus.Left || keyval == IBus.Right || keyval == IBus.Up || keyval == IBus.Down || keyval == IBus.Control_L || keyval == IBus.Control_R || keyval == IBus.Insert || keyval == IBus.Delete || keyval == IBus.Home || keyval == IBus.Page_Up || keyval == IBus.Page_Down || keyval == IBus.End || keyval == IBus.Alt_L || keyval == IBus.Alt_R) {
                 let bntext = Avroparser.parse(engine.buffertext);
                 bntext = utfconv.utf8Decode(bntext);
                 let text = IBus.Text.new_from_string(bntext);
@@ -97,10 +95,10 @@ if( bus.is_connected() ) {
                 engine.hide_preedit_text();
                 engine.hide_auxiliary_text();
             }
-            return false ;
+            return false;
         });
 
-        engine.connect('focus-out', function (){
+        engine.connect('focus-out', function () {
             if (engine.buffertext.length > 0) {
                 let bntext = Avroparser.parse(engine.buffertext);
                 bntext = utfconv.utf8Decode(bntext);
@@ -116,36 +114,36 @@ if( bus.is_connected() ) {
         return engine;
     }
 
-    var factory = IBus.Factory.new( bus.get_connection() );
+    var factory = IBus.Factory.new(bus.get_connection());
     //factory.add_engine("avro-phonetic",GObject.type_from_name('IBusEngine'));
     factory.connect('create-engine', _create_engine_cb);
     var component = new IBus.Component({
-        name:"org.freedesktop.IBus.Avro",
-        description:"Avro phonetic",
-        version:"0.9",
-        license:"MPL",
-        author:"Sarim Khan <sarim2005@gmail.com>",
-        homepage:"https://github.com/sarim/ibus-avro",
+        name: "org.freedesktop.IBus.Avro",
+        description: "Avro phonetic",
+        version: "0.9",
+        license: "MPL",
+        author: "Sarim Khan <sarim2005@gmail.com>",
+        homepage: "https://github.com/sarim/ibus-avro",
         exec: eevars.get_libexecdir() + "/main-gjs.js",
-        textdomain:"avro-phonetic"
+        textdomain: "avro-phonetic"
     });
 
     var avroenginedesc = new IBus.EngineDesc({
-        name:"avro-phonetic",
-        longname:"avro phonetic",
-        description:"Avro Phonetic Engine",
-        language:"bn",
-        license:"MPL",
-        author:"Sarim Khan <sarim2005@gmail.com>",
+        name: "avro-phonetic",
+        longname: "avro phonetic",
+        description: "Avro Phonetic Engine",
+        language: "bn",
+        license: "MPL",
+        author: "Sarim Khan <sarim2005@gmail.com>",
         icon: eevars.get_pkgdatadir() + "/avro-bangla.png",
-        layout:"bn"
+        layout: "bn"
     });
 
-    component.add_engine (avroenginedesc);
+    component.add_engine(avroenginedesc);
     if (exec_by_ibus) {
-        bus.request_name ("org.freedesktop.IBus.Avro",0);
+        bus.request_name("org.freedesktop.IBus.Avro", 0);
     } else {
-        bus.register_component (component);
+        bus.register_component(component);
     }
     IBus.main();
 }

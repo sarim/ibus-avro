ba0348484a5ca0bc8f47469695a03f7345a11705
Mehdi Hasan <mhasan@omicronlab.com>
2012-06-29 08:29:44 +0600

    Added MPL headers to new files
    Signed-off-by: Mehdi Hasan <mhasan@omicronlab.com>

--- avroregexlib.js
+++ avroregexlib.js
@@ -1,4 +1,30 @@
-//#!/usr/bin/env gjs
+/*
+    =============================================================================
+    *****************************************************************************
+    The contents of this file are subject to the Mozilla Public License
+    Version 1.1 (the "License"); you may not use this file except in
+    compliance with the License. You may obtain a copy of the License at
+    http://www.mozilla.org/MPL/
+
+    Software distributed under the License is distributed on an "AS IS"
+    basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+    License for the specific language governing rights and limitations
+    under the License.
+
+    The Original Code is jsAvroPhonetic
+
+    The Initial Developer of the Original Code is
+    Mehdi Hasan Khan <mhasan@omicronlab.com>
+    Rifat Nabi <to.rifat@gmail.com>
+
+    Copyright (C) OmicronLab (http://www.omicronlab.com). All Rights Reserved.
+
+
+    Contributor(s): ______________________________________.
+
+    *****************************************************************************
+    =============================================================================
+*/
 
 imports.searchPath.unshift('.');
 const utfconv = imports.utf8;
--- dbsearch.js
+++ dbsearch.js
@@ -1,4 +1,29 @@
-#!/usr/bin/env gjs
+/*
+    =============================================================================
+    *****************************************************************************
+    The contents of this file are subject to the Mozilla Public License
+    Version 1.1 (the "License"); you may not use this file except in
+    compliance with the License. You may obtain a copy of the License at
+    http://www.mozilla.org/MPL/
+
+    Software distributed under the License is distributed on an "AS IS"
+    basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+    License for the specific language governing rights and limitations
+    under the License.
+
+    The Original Code is jsAvroPhonetic
+
+    The Initial Developer of the Original Code is
+    Mehdi Hasan Khan <mhasan@omicronlab.com>
+
+    Copyright (C) OmicronLab (http://www.omicronlab.com). All Rights Reserved.
+
+
+    Contributor(s): ______________________________________.
+
+    *****************************************************************************
+    =============================================================================
+*/
 
 
 imports.searchPath.unshift('.');
@@ -148,5 +173,5 @@ DBSearch.prototype = {
 /* --------- */
 /* Test code */
 /* --------- */
-var __dbSearch = new DBSearch ();
-__dbSearch._printWords('u');
+// var __dbSearch = new DBSearch ();
+// __dbSearch._printWords('u');

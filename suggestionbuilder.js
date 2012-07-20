efdfd16c140ce5ea56bff05a62024a601f48663e
Mehdi Hasan <mhasan@omicronlab.com>
2012-07-20 16:36:09 +0600

    Hotfix: candidate option loading changed to synchronized i/o again
    Signed-off-by: Mehdi Hasan <mhasan@omicronlab.com>

--- suggestionbuilder.js
+++ suggestionbuilder.js
@@ -371,12 +371,13 @@ SuggestionBuilder.prototype = {
             var file = gio.File.new_for_path(GLib.get_home_dir() + "/.candidate-selections.json");
         
             if (file.query_exists (null)) {
-                /*
+                
                 var file_stream = file.read(null);
                 var data_stream = gio.DataInputStream.new(file_stream);
                 var json = data_stream.read_until("", null);
                 this._candidateSelections = JSON.parse(json[0]);
-                */
+                
+                /*
                 file.read_async(0, null,
                 		function(source, result){
                 		    var file_stream = source.read_finish(result);
@@ -389,6 +390,7 @@ SuggestionBuilder.prototype = {
                 		        this._logger(e, 'Error in _loadCandidateSelectionsFromFile');
                 		    }
                 		});
+                */
             } else {
                 this._candidateSelections = {};
             }

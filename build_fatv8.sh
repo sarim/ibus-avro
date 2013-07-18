c8a3395eeed89739ec5c0835906cda58ed5998d4
Sarim Khan <sarim2005@gmail.com>
2013-07-18 03:03:42 -0700

    build script update

--- build_fatv8.sh
+++ build_fatv8.sh
@@ -1,9 +1,9 @@
-export CC=`which clang`
-export CXX=`which clang++`
-export GYP_DEFINES="clang=1"
+#export CC=`which clang`
+#export CXX=`which clang++`
+#export GYP_DEFINES="clang=1"
 
 
-AVROLIBS="platform utf8 suffixdict autocorrect avrodict levenshtein avrolib avroregexlib dbsearch gi suggestionbuilder"
+AVROLIBS="platform utf8 suffixdict autocorrect avrodict levenshtein avrolib avroregexlib dbsearch gi suggestionbuilder export"
 
 cd avrov8/lib
 > ../avrolibcombined.js
@@ -12,15 +12,17 @@ do
 echo \; modulename = \"$LIBNAME\" \; >> ../avrolibcombined.js
 cat $LIBNAME.js >> ../avrolibcombined.js
 done
+
 cd ..
 native2ascii avrolibcombined.js ../avrolibv8.js
 cd ..
 
 cd v8-3.18.1
+make clean
 make dependencies
 
 
-make native -j 4 library=shared werror=no
+make native -j 4 library=shared werror=no snapshot=off
 cp out/native/lib.target/libv8.so ../libs/
 
 
